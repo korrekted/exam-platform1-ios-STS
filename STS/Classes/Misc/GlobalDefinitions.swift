36646a623f67f3dc53ec41eb48425a8c292d028c
akonst17 <akonst17@gmail.com>
2022-07-04 17:11:51 +0300

    new domain

--- STS/Classes/Misc/GlobalDefinitions.swift
+++ STS/Classes/Misc/GlobalDefinitions.swift
@@ -6,7 +6,7 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "QB8GkNHP6QGEEztd"
 
     static let sdkDomainUrl = "https://analytics.korrekted.com"
